e274f9384015ab9aa779b8871f581a95c82298b6
AgusMott <motturaagustin@gmail.com>
2023-06-29 14:46:15 -0300

    ya casi

--- src/app/grilla/grilla.component.ts
+++ src/app/grilla/grilla.component.ts
@@ -21,10 +21,9 @@ export class GrillaComponent implements OnInit {
   }
  
   AgregarALista() {
-      //this.servicioCorredores.addCorredor(this.corredor);
       this.servicioCorredores.disparadorCorredores.emit(
         { data: this.dataEntrante }
       )
-    }
-
+      
+  }
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,7 +1,5 @@
 import { Component,OnInit} from '@angular/core';
-import {Corredor} from '../app.component';
 import { ServicioCorredores } from '../service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -13,15 +11,15 @@ export class ListaComponent implements OnInit{
 
   public corredores:Array<any> = [];
   
-  constructor(private servicioCorredores: ServicioCorredores, private router: Router) { }
+  constructor(private servicioCorredores: ServicioCorredores) { }
   ngOnInit(): void {
     this.servicioCorredores.disparadorCorredores.subscribe(
       data => { this.corredores.push(data) }
     )
   }
-  //corredores!:Corredor[];
-  borrarCorredor(Corredor: any){
-    let index = this.corredores.indexOf(Corredor);
-    this.corredores.splice(index,1);
+  
+  borrarCorredor(corredor: any){
+    let index = this.corredores.indexOf(corredor);
+    this.corredores.splice(index, 1);
   }
 }
